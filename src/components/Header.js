6a81ebafe6fa7dd76dea4654f10bb92797b1c197
Hridibrata Saha <hridibrata@gmail.com>
2025-05-23 12:25:17 +0530

    finaaly looks like the header is complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,33 @@
-import React from 'react'
-import './Header.css'
-import { useState } from 'react';
+import React, { useState } from 'react';
+import './Header.css'; 
 
-const Header = () => {
 
-     const [city, setCity] = useState("");
+const Header = ({ handleSearch, loading }) => {
+
+  const [searchCity, setSearchCity] = useState('');
 
   return (
-    <div className="header">
-      <div className="title">
-        AeroLocate
-      </div>
+    <div className="App-header">
+      <div className="title">AeroLocate</div>
       <div className="subtitle">
-        <input 
-           value={city}
-          onChange={(e) => setCity(e.target.value)}
-          type="text" 
-          placeholder="Search for Cities..." 
-          className="search-bar" 
-        />
+        <div className="search-bar-container">
+          <input
+            value={searchCity}
+            onChange={(e) => setSearchCity(e.target.value)}
+            type="text"
+            placeholder="Search for Cities..."
+            className="search-input-field"
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') handleSearch();
+            }}
+          />
+          <button onClick={handleSearch} disabled={loading} className='search-button'>
+            {loading ? 'Searching...' : 'Search'}
+          </button>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-
-export default Header
+export default Header;
